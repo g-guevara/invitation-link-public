cae2c9a17a89299476b02e70196a3a25daa05d79
gguevara112 <guguevara@alumnos.uai.cl>
2025-01-10 13:22:52 -0300

    navitems and alert darkmode

--- config/site.ts
+++ config/site.ts
@@ -17,36 +17,12 @@ export const siteConfig = {
   ],
   navMenuItems: [
     {
-      label: "Profile",
-      href: "/profile",
+      label: "Home",
+      href: "/",
     },
     {
-      label: "Dashboard",
-      href: "/dashboard",
-    },
-    {
-      label: "Projects",
-      href: "/projects",
-    },
-    {
-      label: "Team",
-      href: "/team",
-    },
-    {
-      label: "Calendar",
-      href: "/calendar",
-    },
-    {
-      label: "Settings",
-      href: "/settings",
-    },
-    {
-      label: "Help & Feedback",
-      href: "/help-feedback",
-    },
-    {
-      label: "Logout",
-      href: "/logout",
+      label: "Recursos",
+      href: "/docs",
     },
   ],
   links: {
